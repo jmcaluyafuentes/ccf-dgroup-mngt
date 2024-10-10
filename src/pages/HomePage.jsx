ab75025b8e25df90aa755c1b317fe29ce5d63e12
John Mark Fuentes <jmcaluyafuentes@gmail.com>
2024-10-10 19:03:19 +1100

    setup: set up home page component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom'
 
-const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
+const Home = () => {
+    return (
+        <section className="hero is-fullheight" id="hero">
+            <div className="container" id="herocontainer">
+                <h1 className="title is-1">Welcome to CCF Australia</h1>
+                <h2 className="subtitle is-3 m-5 has-text-weight-bold">Join a care group to connect and grow spiritually.</h2>
+                
+                {/* Call-to-action button linking to the DGroup page */}
+                <Link to="/dgroups" className="button is-warning is-large mt-5" id="cta-search-recipes">
+                    Find a DGroup
+                </Link>
+            </div>
+        </section>
+    );
+};
 
-export default HomePage
+export default Home;
