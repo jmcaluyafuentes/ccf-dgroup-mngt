a0d499aab792bfdf6d8b5c9594ce170b1ab425f3
John Mark Fuentes <jmcaluyafuentes@gmail.com>
2024-10-19 02:18:57 +1100

    chore(DGroupContainer): restructure UI for loading and error states

--- src/containers/DGroupContainer.jsx
+++ src/containers/DGroupContainer.jsx
@@ -17,19 +17,19 @@ const DGroupContainer = () => {
     useEffect(() => {
         // Fetch available options for satellites and life stages
         const fetchOptions = async () => {
-        try {
-            // Fetch all DGroups
-            const data = await fetchDGroups();
+            try {
+                // Fetch all DGroups
+                const data = await fetchDGroups();
 
-            // Extract unique satellite names and life stages
-            const satellites = Array.from(new Set(data.map((group) => group.satellite.name)));
-            const lifeStages = Array.from(new Set(data.map((group) => group.lifeStage)));
+                // Extract unique satellite names and life stages
+                const satellites = Array.from(new Set(data.map((group) => group.satellite.name)));
+                const lifeStages = Array.from(new Set(data.map((group) => group.lifeStage)));
 
-            setAllSatellites(satellites);
-            setAllLifeStages(lifeStages);
-        } catch (err) {
-            console.error(err);
-        }
+                setAllSatellites(satellites);
+                setAllLifeStages(lifeStages);
+            } catch (err) {
+                console.error(err);
+            }
         };
 
         fetchOptions();
@@ -50,26 +50,44 @@ const DGroupContainer = () => {
         setSelectedDGroup(dGroup);
     };
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error loading groups.</p>;
+    const handleRetry = () => {
+        fetchDGroups();
+        setSelectedSatellite('');
+        setSelectedLifeStage('');
+    }
 
     return (
         <div>
-        {!selectedDGroup ? (
-            <>
-            <Filter 
-                satellites={allSatellites}
-                lifeStages={allLifeStages}
-                selectedSatellite={selectedSatellite} 
-                selectedLifeStage={selectedLifeStage} 
-                onSatelliteChange={handleSatelliteChange} 
-                onLifeStageChange={handleLifeStageChange} 
-            />
-            <DGroupList dGroups={dGroups} onSelectDGroup={handleDGroupSelect} />
-            </>
-        ) : (
-            <DGroupDetails dGroup={selectedDGroup} onBack={() => setSelectedDGroup(null)} />
-        )}
+            {!selectedDGroup ? (
+                <>
+                    <Filter 
+                        satellites={allSatellites}
+                        lifeStages={allLifeStages}
+                        selectedSatellite={selectedSatellite} 
+                        selectedLifeStage={selectedLifeStage} 
+                        onSatelliteChange={handleSatelliteChange} 
+                        onLifeStageChange={handleLifeStageChange} 
+                    />
+                    {loading ? (
+                        <p>Loading...</p>
+                    ) : (
+                        error ? (
+                            <div>
+                                <p className="has-text-danger">Error loading groups...</p>
+                                <button onClick={handleRetry} className="button is-normal is-danger mt-2">Retry</button>
+                            </div>
+                        ) : (
+                            dGroups.length > 0 ? (
+                                <DGroupList dGroups={dGroups} onSelectDGroup={handleDGroupSelect} />
+                            ) : (
+                                <p className="has-text-danger">{`Sorry, there are no available DGroups in "${selectedSatellite}" for "${selectedLifeStage}".`}</p>
+                            )
+                        )
+                    )}
+                </>
+            ) : (
+                <DGroupDetails dGroup={selectedDGroup} onBack={() => setSelectedDGroup(null)} />
+            )}
         </div>
     );
 };
