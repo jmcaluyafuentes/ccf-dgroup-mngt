bc5dbef1595062da991d2637d5a9f54c2c6b4c28
John Mark Fuentes <jmcaluyafuentes@gmail.com>
2024-10-19 16:16:55 +1100

    chore(DGroupDetails): adjust font size and add place holder text to description and leader

--- src/components/DGroupDetails.jsx
+++ src/components/DGroupDetails.jsx
@@ -5,15 +5,15 @@ const DGroupDetails = ({ dGroup, onBack }) => {
         <div className="card-content card-detail">
             <h2 className="title is-4">{dGroup.name}</h2>
 
-            <h3 className="title is-6 mt-5 mb-1">Life Stage: {dGroup.lifeStage}</h3>
-            <h3 className="title is-6 mt-1">Satellite: {dGroup.satellite.name}</h3>
-            <p className="subtitle is-6 mt-1">Description: {dGroup.description}</p>
+            <h3 className="title title-detail is-6 mt-5 mb-1">Life Stage: {dGroup.lifeStage}</h3>
+            <h3 className="title title-detail is-6 mt-1">Satellite: {dGroup.satellite.name}</h3>
+            <p className="subtitle is-6 mt-3">Description: We encourage and pray for one another. We also do community service and share the gospel.{dGroup.description}</p>
 
             <p className="subtitle is-6 mt-5">Meeting Day: {dGroup.meetingDay}</p>
             <p className="subtitle is-6 mt-1">Meeting Time: 5 PM</p>
             <p className="subtitle is-6 mt-1">Meeting Frequency: {dGroup.meetingFrequency}</p>
 
-            <p className="subtitle is-6 mt-5">Leader: {dGroup.leader}</p>
+            <p className="subtitle is-6 mt-5">Leader: John Doe{dGroup.leader}</p>
             <p className="subtitle is-6 mt-1">Mobile No.: 123456</p>
             <p className="subtitle is-6 mt-1 mb-5">Email: leader@email.com</p>
 
