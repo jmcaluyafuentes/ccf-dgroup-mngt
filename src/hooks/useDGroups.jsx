93e21bfbb343bb668364d9f3232da26d420bbff3
John Mark Fuentes <jmcaluyafuentes@gmail.com>
2024-10-22 22:54:35 +1100

    chore(useDGroups): set initial value of loading state to be true to reduce unncessary re-rendering

--- src/hooks/useDGroups.jsx
+++ src/hooks/useDGroups.jsx
@@ -3,13 +3,12 @@ import { fetchDGroups } from '../api/dgroupApi';
 
 const useDGroups = ({ satellite, lifeStage }) => {
     const [dGroups, setDGroups] = useState([]);
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
         
         const getDGroups = async () => {
-            setLoading(true);
             try {
                 // Simulate delay in receiving response data from API call to check UI behavior
                 // await new Promise((resolve) => setTimeout(resolve, 1000))
